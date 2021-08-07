84ec205896f8b3b3885d18d11d8849b565bde731
Nishant <nishantbhosale244@gmail.com>
2021-08-07 17:36:47 +0530

    Added Orders page

--- src/components/PopupBar/PopupBar.js
+++ src/components/PopupBar/PopupBar.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./PopupBar.css";
+import { Fade } from "react-reveal";
 
 const PopupBar = (props) => {
 	let classes = ["popup-bar"];
 
 	if (props.success) classes.push("success");
 	else if (props.fail) classes.push("fail");
-	return <div className={classes.join(" ")}>{props.children}</div>;
+	return (
+		<Fade>
+			<div className={classes.join(" ")}>{props.children}</div>
+		</Fade>
+	);
 };
 
 export default PopupBar;
---
+++ src/containers/OrdersPage/OrdersPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OrdersPage = () => {
+	return <div></div>;
+};
+
+export default OrdersPage;
